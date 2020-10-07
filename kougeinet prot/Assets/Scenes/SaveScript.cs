3d3c3d5927f0f39bdf10c80b903780375107447d
Yamato-0v0 <g1827022@st.t-kougei.ac.jp>
2020-10-07 12:47:23 +0900

    20201007 1st

---
+++ kougeinet prot/Assets/Scenes/SaveScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveScript : MonoBehaviour
+{
+    List<int> intList = new List<int>();
+    List<string> textList = new List<string>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //for (int i = 0; i < intlist.Count; i++)
+        //{
+        //    intlist[i] = PlayerPrefs.GetInt("", 1);
+        //}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void AddSave()
+    {
+        //textList.Add(ItemGenerator.textIn);
+    }
+}
